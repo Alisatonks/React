1cb3e963d3cfb576a744a1f139bfd280bbd08c06
Volha Haiduk <Alisatonks2385@gmail.com>
2023-03-15 13:02:33 +0100

    feat: add styles and adaptive  on error page

--- y/src/pages/Error.tsx
+++ y/src/pages/Error.tsx
@@ -4,10 +4,19 @@ import { Link } from 'react-router-dom';
 class Error extends React.PureComponent {
   render() {
     return (
-      <>
-        <h1>This is an error page</h1>
-        <Link to="/">Go Home</Link>
-      </>
+      <div className="error">
+        <h1 className="error__h1">This is an error page</h1>
+        <Link to="/" className="error__link">
+          Go Home
+        </Link>
+        <div className="error__sloth-cont">
+          <img
+            src="../src/assets/png/sloth.png"
+            className="error__sloth"
+            alt="404_img"
+          />
+        </div>
+      </div>
     );
   }
 }
