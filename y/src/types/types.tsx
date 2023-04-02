b3cbd97d7272dbeca7421674885c457c276e11e7
Volha Haiduk <Alisatonks2385@gmail.com>
2023-04-02 21:58:59 +0200

    refactor: delete unused types

--- y/src/types/types.tsx
+++ y/src/types/types.tsx
@@ -16,35 +16,10 @@ export type ICardProps2 = Pick<
   'category' | 'discount' | 'rating' | 'stock'
 >;
 
-export interface IState {
-  inputText: string;
-}
-
-export type IProps = {
-  key: string;
-};
-
-export type IH1Content = {
-  content: string;
-};
-
 export type ImgProps = {
   source: string | boolean;
   alt: string;
   myClass: string;
-};
-
-export type IFormProps = {
-  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-  inputText: React.RefObject<HTMLInputElement>;
-  inputDate: React.RefObject<HTMLInputElement>;
-  inputDrop: React.RefObject<HTMLSelectElement>;
-  concent: React.RefObject<HTMLInputElement>;
-  radioReturn: React.RefObject<HTMLInputElement>;
-  radioExchange: React.RefObject<HTMLInputElement>;
-  inputFile: React.RefObject<HTMLInputElement>;
-  formRef: React.RefObject<HTMLFormElement>;
-  cardCreated: boolean;
 };
 
 export type IFormCardData = {
@@ -55,27 +30,6 @@ export type IFormCardData = {
   concent: boolean;
   radio: string | boolean;
   inputFile: string | boolean;
-};
-
-export type IFormPageState = {
-  formCards: IFormCardData[];
-  validationErrors: string[];
-};
-
-export type IFormContainerProps = {
-  addCard: (data: IFormCardData) => void;
-  addValErr: (data: string[]) => void;
-};
-
-export type InputRadioProps = {
-  htmlFor: string;
-  content: string;
-  ref: React.RefObject<HTMLInputElement>;
-  id: string;
-};
-
-export type CardContainerState = {
-  cardCreated: boolean;
 };
 
 export type IFormValues = {
